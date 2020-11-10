f7b109f7273b86f31f4283ac639dc41a1ddf2f94
Anton Shimanovich <ashimanovich127@gmail.com>
2020-11-10 21:34:50 +0300

    first_task

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 "use strict";
 
-console.log('Hello');
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", '');
+
+// let movie,
+//     value,
+//     test,
+//     arr;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+for (let i = 0; i < 2; i++) {
+    let movie = prompt("Один из последних просмотренных фильмов", '');
+    let value = prompt("На сколько оцените его?", '');
+    personalMovieDB.movies[movie] = value;
+}
+
+console.log(personalMovieDB);
