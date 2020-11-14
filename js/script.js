b2bb51e11434ac3f5f359543bac26031ebb335ba
Anton Shimanovich <ashimanovich127@gmail.com>
2020-11-14 23:39:57 +0300

    Second task

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", '');
+let numberOfFilms;
+do {
+    numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", '');
+} while (numberOfFilms == '' || numberOfFilms == null);
 
-// let movie,
-//     value,
-//     test,
-//     arr;
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -14,10 +13,27 @@ const personalMovieDB = {
     private: false
 };
 
+if (personalMovieDB.count < 10) {
+    alert("Просмотренно довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
+let movie,
+    value;
+
 for (let i = 0; i < 2; i++) {
-    let movie = prompt("Один из последних просмотренных фильмов", '');
-    let value = prompt("На сколько оцените его?", '');
-    personalMovieDB.movies[movie] = value;
+    movie = prompt("Один из последних просмотренных фильмов", '');
+    value = prompt("На сколько оцените его?", '');
+    if (movie != '' && value != '' && movie != null && value != null && movie.length < 50) {
+        personalMovieDB.movies[movie] = value;
+    } else {
+        i--;
+    }
 }
 
 console.log(personalMovieDB);
