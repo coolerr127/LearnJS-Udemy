fd9746eb9d30214d523f1b734a0ac5ecb2936ec3
Anton Shimanovich <ashimanovich127@gmail.com>
2021-06-09 16:04:23 +0300

    calculator development started

--- proj1/js/script.js
+++ proj1/js/script.js
@@ -475,4 +475,37 @@ document.addEventListener('DOMContentLoaded', () => {
     // nextBtn.addEventListener('click', () => {
     //     changeSlide(1);
     // });
+
+    //=====CALCULATOR=====
+
+    const result = document.querySelector('.calculating__result span');
+    let sex, height, weight, age, ratio;
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____';
+            return;
+        }
+
+        if (sex === 'female') {
+            result.textContent = (447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio;
+        } else {
+            result.textContent = (88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio;
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        document.querySelector(parentSelector).addEventListener('click', (e) => {
+            if (e.target.getAttribute('data-ratio')) {
+                ratio = +e.target.getAttribute('data-ratio');
+            } else {
+                sex = e.target.getAttribute('id');
+            }
+            console.log(ratio, sex);
+        });
+    }
 });
