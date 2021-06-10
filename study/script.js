a81fb656951d3f1eab8b6a841a2cbc0a08cd0bb5
Anton Shimanovich <ashimanovich127@gmail.com>
2021-06-11 00:13:55 +0300

    les69 encapsulation

--- study/script.js
+++ study/script.js
@@ -116,9 +116,72 @@ const person = {
 //\D - не числа, \W, \S - не буква, не пробел
 // flags: i - вне зависимости от регистра, g - несколько вхождений, m - многострочный режим
 
-const ans = prompt('Введите имя');
+// const ans = prompt('Введите имя');
 
 const reg = /n/i;
 
-console.log(reg.test(ans)); //вернет true/false есть/нет совпадений
+// console.log(reg.test(ans)); //вернет true/false есть/нет совпадений
 // console.log(ans.search(reg));
+
+//=====ENCAPSULATION=====
+
+// function User(name, age) {
+//     this.name = name;
+//     let userAge = age;
+
+//     this.say = function() {
+//         console.log(`Имя пользователя: ${this.name}, возраст: ${userAge}`);
+//     };
+
+//     this.getAge = function() {
+//         return userAge;
+//     };
+
+//     this.setAge = function(age) {
+//         if (typeof age === 'number' && age > 0 && age < 110) {
+//             userAge = age;
+//         } else {
+//             console.log('Недопустимое значение');
+//         }
+//     };
+// }
+// class User {
+//     constructor(name, age) {
+//         this.name = name;
+//         this._age = age;
+//     }
+
+//     #surname = 'Petrychenko';
+
+//     say = () => {
+//         console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст ${this._age}`);
+//     }
+//     set age(age) {
+//         if (typeof age === 'number' && age > 0 && age < 110) {
+//             this._age = age;
+//         } else {
+//             console.log('Недопустимое значение!');
+//         }
+//     }
+
+//     get age() {
+//         return this._age;
+//     }
+
+//     set surname(surname) {
+//         this.#surname = surname;
+//     }
+
+//     get surname() {
+//         return this.#surname;
+//     }
+// }
+
+// const ivan = new User('Ivan', 27);
+
+// // console.log(ivan.getAge());
+// // ivan.setAge(23);
+// // console.log(ivan.getAge());
+// ivan.surname = 'haha'
+// console.log(ivan.surname);
+// ivan.say();
