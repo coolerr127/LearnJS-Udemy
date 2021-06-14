8a2e490c6cb5f3b4eee67c1850f6575b725da409
Anton Shimanovich <ashimanovich127@gmail.com>
2021-06-14 23:14:05 +0300

    les79 function generator

--- study/script.js
+++ study/script.js
@@ -202,3 +202,15 @@ const reg = /n/i;
 // }
 
 // console.log('Still normal');
+
+//=====FUNCTION GENERATOR=====
+
+// function* count(n) {
+//     for (let i = 0; i < n; i++) {
+//         yield i;
+//     } 
+// }
+
+// for (let i of count(7)) {
+//     console.log(i);
+// }
